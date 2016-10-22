96717a282aa2a169aa332fd39cf5147f7d0fddbe
Dang <letatdang@gmail.com>
2016-10-22 09:53:51 +0700

    UML class
    I created based classes in package BusinessLogic

---
+++ BitTorrent-like/src/BusinessLogic/File.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class File {
+    private List<Chunk> chunks;
+    private String FileName;
+}
---
+++ BitTorrent-like/src/BusinessLogic/Machine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author admin
+ */
+public class Machine {
+    private String IPaddr;
+    private Vector<File> Files;
+    private Vector<Machine> ConnectedMachines;
+}
