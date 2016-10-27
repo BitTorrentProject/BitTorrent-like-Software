9fd40c93ccace6baa9774a3276217e82a97b379d
Thien Nguyen <masterofdark59@gmail.com>
2016-10-27 11:41:27 +0700

    Not changed

--- BitTorrent-like/src/BusinessLogic/UploadingFile.java
+++ BitTorrent-like/src/BusinessLogic/UploadingFile.java
@@ -77,6 +77,8 @@ public class UploadingFile {
       FileChannel destChannel = null;
       try {
           sourceChannel = new FileInputStream(source).getChannel();
+          sourceChannel = new FileInputStream(source).getChannel();
+          
           destChannel = new FileOutputStream(dest).getChannel();
           destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
          }finally{
