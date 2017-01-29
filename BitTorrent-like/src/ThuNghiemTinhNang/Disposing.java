b9eaf7009419d9035c4f75c0c4b5121c2ae86ffb
Dang <letatdang@gmail.com>
2017-01-29 14:04:41 +0700

    remove unused comments

--- BitTorrent-like/src/ThuNghiemTinhNang/Disposing.java
+++ BitTorrent-like/src/ThuNghiemTinhNang/Disposing.java
@@ -47,36 +47,7 @@ public class Disposing {
         t = new Thread(Thread2);
         t.start();
         t.interrupt();
-        //t.start();
         
-        /*Runnable Thread1 = new Runnable() {
-            int t = 0;
-            public Thread T1 = new Thread(this);
-            public void run() {
-                while (true) {
-                    int S = 0;
-                    for (int j = 0; j < 10; j++) {
-                        S += j;
-                    }
-                    System.out.println("end thread 1 " + S);
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(Disposing.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    synchronized (Thread2) {
-                        if (isAlive()) {
-                            Thread2.notifyAll();
-                            for (int i = 0; i < 5; i++) {
-                                System.out.println("T1 " + i);
-                            }
-                            return;
-                        } else {
-                        }
-                    }
-                }
-            }
-        };*/
         
         t.join();
        
